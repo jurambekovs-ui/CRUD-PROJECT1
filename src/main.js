a79e50904b17fe1a61931f51b9e8d01b698b8d5f
Shoxjaxon <jurambekovs@gmail.com>
2025-11-26 10:09:34 +0500

    Refactor main.js to use new router structure

--- src/main.js
+++ src/main.js
@@ -1,26 +1,18 @@
 import express from 'express';
 import { config } from 'dotenv';
-import connectDB from './config/db.js';         
-import categoryRoutes from './routes/category.route.js';  
-
 config();
 
+import connectDB from './config/db.js';
+import router from './routes/index.route.js';
+
+
 const app = express();
-const PORT = Number(process.env.PORT) || 2000;
+const PORT = Number(process.env.PORT);
 
 app.use(express.json());
 
-app.use('/api/categories', categoryRoutes);   
+await connectDB();
 
-const startServer = async () => {
-    try {
-        await connectDB();    
-        app.listen(PORT, () => {
-            console.log(`Server ${PORT}-p`);
-        });
-    } catch (err) {
-        console.error("Server ishga tushmadi:", err);
-    }
-};
+app.use('/api', router);
 
-startServer();
+app.listen(PORT, () => console.log('server running on port', PORT));
